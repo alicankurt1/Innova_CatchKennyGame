83a1cf592b41f71e37e989b78ee244de00af7949
Alican Kurt <kurta15@itu.edu.tr>
2021-12-28 14:57:16 +0300

    Initial Commit

---
+++ Innova_CatchKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Innova_CatchKennyGame
+//
+//  Created by Alican Kurt on 28.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
